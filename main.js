8f78d7544b9677b50ad9c95e6669a1287549796e
baimenov <baimenov@uw.edu>
2018-02-25 22:24:29 -0800

    First Commit

---
+++ main.js
@@ -0,0 +1,315 @@
+function Grid(game) {
+	this.game = game;
+	this.matrix = new Array(100);
+	this.nextMatrix = new Array(100);
+	this.loaded = false;
+	this.dim = 100;
+	
+	for (var i = 0; i < this.matrix.length; i++) {
+		this.matrix[i] = new Array(100);
+		this.nextMatrix[i] = new Array(100);
+	}
+	for (var i = 0; i < this.matrix.length; i++) {
+		for (var j = 0; j < this.matrix[i].length; j++) {
+			this.matrix[i][j] = new Cell(this.game, i, j, 10);
+			this.nextMatrix[i][j] = 0;
+			this.game.addEntity(this.matrix[i][j]);
+		}
+	}
+	Entity.call(this, game, 0, 0);
+}
+
+Grid.prototype = new Entity();
+Grid.prototype.constructor = Grid;
+
+var makeSpaceShip = function() {
+	 var ss = new Array(11);
+	for (var i = 0; i < ss.length; i++) {
+		ss[i] = new Array(20);
+	}
+	for (var i = 0; i < 6; i++) {
+		for (var j = 0; j < ss[i].length; j++) {
+			if ((i === 0 && (j === 1 || j === 4)) ||
+				(i === 1 && j === 0) ||
+				(i === 2 && (j === 0 || j ===4)) ||
+				(i === 3 && ((j >= 0 && j <= 3) || (j === 13 || j === 14))) ||
+				(i === 4 && (j === 6 || j === 7 || j === 8 || j === 14 || j === 15)) ||
+				(i === 5 && (j === 6 || j === 7 || j === 9 || j === 10 || j === 17 || j === 18 || j === 19))) {
+				ss[i][j] = 1;
+				ss[10 - i][j] = 1;
+			} else {
+				ss[i][j] = 0;
+				ss[10 - i][j] = 0;
+			}
+		}
+	}
+	return ss;
+}
+
+Grid.prototype.update = function() {
+	if (this.loaded) {
+		for (var i = 0; i < this.matrix.length; i++) {
+			for (var j = 0; j < this.matrix[i].length; j++) {
+				var me = this.matrix[i][j];
+				var neighs = this.countAliveNeighbors(me);
+				if (neighs < 2 || neighs > 3) {
+					this.nextMatrix[i][j] = 0;
+				} else if ((me.isAlive === 1 && (neighs === 2 || neighs === 3)) ||
+							me.isAlive === 0 && neighs === 3) {
+					this.nextMatrix[i][j] = 1;
+				}
+			}
+		}
+		for (var i = 0; i < this.matrix.length; i++) {
+			for (var j = 0; j < this.matrix[i].length; j++) {
+				this.matrix[i][j].isAlive = this.nextMatrix[i][j];
+			}
+		}
+	} else {
+		if (this.game.click) {
+			var x = Math.floor(this.game.click.x / 10);
+			var y = Math.floor(this.game.click.y / 10);
+			this.matrix[x][y].isAlive = this.matrix[x][y].isAlive === 1 ? 0 : 1;
+			this.game.click = null;
+		}
+	}
+}
+
+Grid.prototype.draw = function(ctx) {
+	for (var i = 0; i <= this.matrix.length; i++) {
+		ctx.beginPath();
+		ctx.lineWidth = 0.1;
+		ctx.moveTo(i * 10, 0);
+		ctx.lineTo(i * 10, 1000);
+		ctx.stroke();
+		ctx.moveTo(0, i * 10);
+		ctx.lineTo(1000, i * 10);
+		ctx.stroke();
+	}
+}
+
+Grid.prototype.clear = function() {
+	for (var i = 0; i < this.matrix.length; i++) {
+		for (var j = 0; j < this.matrix[0].length; j++) {
+			this.matrix[i][j].isAlive = 0;
+			this.nextMatrix[i][j] = 0;
+		}
+	}
+}
+
+Grid.prototype.makeGosper = function() {
+	for (var i = 0; i < this.matrix.length; i++) {
+		for (var j = 0; j < this.matrix[i].length; j++) {
+			if (i === 11) {
+				if (j === 50) {
+					this.matrix[j][i].isAlive = 1;
+				} else {
+					this.matrix[j][i].isAlive = 0;
+				}
+			} else if (i === 12) {
+				if (j === 48 || j === 50) {
+					this.matrix[j][i].isAlive = 1;
+				} else {
+					this.matrix[j][i].isAlive = 0;
+				}
+			} else if (i === 13) {
+				if (j === 47 || j === 46 || j === 39
+					|| j === 38 || j === 60 || j === 61) {
+					this.matrix[j][i].isAlive = 1;
+				} else {
+					this.matrix[j][i].isAlive = 0;
+				}
+			} else if (i === 14) {
+				if (j === 61 || j === 60 || j === 47 || j === 46
+					|| j === 41 || j === 37) {
+					this.matrix[j][i].isAlive = 1;
+				} else {
+					this.matrix[j][i].isAlive = 0;
+				}
+			} else if (i === 15) {
+				if (j === 47 || j === 46 || j === 42 || j === 36
+					|| j === 27 || j === 26) {
+					this.matrix[j][i].isAlive = 1;
+				} else {
+					this.matrix[j][i].isAlive = 0;
+				}
+			} else if (i === 16) {
+				if (j === 50 || j === 48 ||  j === 43 || j === 42
+					|| j === 40 || j === 36 || j === 27 || j === 26) {
+					this.matrix[j][i].isAlive = 1;
+				} else {
+					this.matrix[j][i].isAlive = 0;
+				}
+			} else if (i === 17) {
+				if (j === 50 || j === 42 || j === 36) {
+					this.matrix[j][i].isAlive = 1;
+				} else {
+					this.matrix[j][i].isAlive = 0;
+				}
+			} else if (i === 18) {
+				if (j === 41 || j === 37) {
+					this.matrix[j][i].isAlive = 1;
+				} else {
+					this.matrix[j][i].isAlive = 0;
+				}
+			} else if (i === 19) {
+				if (j === 38 || j === 39) {
+					this.matrix[j][i].isAlive = 1;
+				} else {
+					this.matrix[j][i].isAlive = 0;
+				}
+			} else {
+				this.matrix[j][i].isAlive = 0;
+			}
+			this.nextMatrix[j][i] = this.matrix[j][i].isAlive;
+		}
+	}
+}
+
+Grid.prototype.randomize = function() {
+	for (var i = 0; i < this.matrix.length; i++) {
+		for (var j = 0; j < this.matrix[i].length; j++) {
+			var next = Math.random();
+			this.matrix[i][j].isAlive = next < 0.3 ? 1 : 0;
+			this.nextMatrix[i][j] = this.matrix[i][j].isAlive;
+		}
+	}
+}
+
+Grid.prototype.countAliveNeighbors = function(me) {
+	var result = 0;
+	if (me.x === 0 && me.y === 0) {
+		result += (this.matrix[me.x + 1][me.y + 1].isAlive +
+					this.matrix[me.x + 1][me.y].isAlive + 
+					this.matrix[me.x][me.y + 1].isAlive);
+
+	} else if (me.x === this.dim - 1 && me.y === 0) {
+		result += (this.matrix[me.x - 1][me.y].isAlive +
+					this.matrix[me.x - 1][me.y + 1].isAlive +
+					this.matrix[me.x][me.y + 1].isAlive);
+	} else if (me.x === this.dim - 1 && me.y === this.dim - 1) {
+		result += (this.matrix[me.x][me.y - 1].isAlive +
+					this.matrix[me.x - 1][me.y - 1].isAlive +
+					this.matrix[me.x - 1][me.y].isAlive);
+	} else if (me.x === 0 && me.y === this.dim - 1) {
+		result += (this.matrix[me.x][me.y - 1].isAlive +
+					this.matrix[me.x + 1][me.y - 1].isAlive +
+					this.matrix[me.x + 1][me.y].isAlive);
+	} else if (me.x === 0) {
+		result += (this.matrix[me.x][me.y - 1].isAlive +
+					this.matrix[me.x + 1][me.y - 1].isAlive +
+					this.matrix[me.x + 1][me.y].isAlive +
+					this.matrix[me.x + 1][me.y + 1].isAlive + 
+					this.matrix[me.x][me.y + 1].isAlive);
+	} else if (me.x === this.dim - 1) {
+		result += (this.matrix[me.x][me.y - 1].isAlive +
+					this.matrix[me.x - 1][me.y - 1].isAlive +
+					this.matrix[me.x - 1][me.y].isAlive +
+					this.matrix[me.x - 1][me.y + 1].isAlive + 
+					this.matrix[me.x][me.y + 1].isAlive);
+	} else if (me.y === 0) {
+		result += (this.matrix[me.x + 1][me.y].isAlive +
+					this.matrix[me.x + 1][me.y + 1].isAlive +
+					this.matrix[me.x][me.y + 1].isAlive +
+					this.matrix[me.x - 1][me.y + 1].isAlive + 
+					this.matrix[me.x - 1][me.y].isAlive);
+	} else if (me.y === this.dim - 1) {
+		result += (this.matrix[me.x + 1][me.y].isAlive +
+					this.matrix[me.x + 1][me.y -1].isAlive +
+					this.matrix[me.x][me.y - 1].isAlive +
+					this.matrix[me.x - 1][me.y - 1].isAlive + 
+					this.matrix[me.x - 1][me.y].isAlive);
+	} else {
+		
+		result += (this.matrix[me.x + 1][me.y].isAlive +
+					this.matrix[me.x + 1][me.y - 1].isAlive +
+					this.matrix[me.x][me.y - 1].isAlive +
+					this.matrix[me.x - 1][me.y - 1].isAlive + 
+					this.matrix[me.x - 1][me.y].isAlive +
+					this.matrix[me.x - 1][me.y + 1].isAlive +
+					this.matrix[me.x][me.y + 1].isAlive +
+					this.matrix[me.x + 1][me.y + 1].isAlive);
+	}
+	return result;
+}
+
+Grid.prototype.placeSetup = function(arr, x, y) {
+	for (var i = 0; i < arr.length; i++) {
+		for (var j = 0; j < arr[0].length; j++) {
+			this.matrix[j + x][i + y].isAlive = arr[i][j];
+		}
+	}
+}
+
+function Cell(game, x, y, width) {
+	//1 = alive = black
+	//0 = dead  = white
+	this.isAlive = 0;
+	this.width = width;
+	Entity.call(this, game, x, y);
+}
+
+Cell.prototype = new Entity();
+Cell.prototype.constructor = Cell;
+
+Cell.prototype.update = function() {
+
+}
+
+Cell.prototype.draw = function(ctx) {
+	ctx.fillStyle = this.isAlive === 1 ? "black" : "white";
+
+	ctx.fillRect(this.x * this.width, this.y * this.width, this.width, this.width);
+	Entity.prototype.draw.call(this, ctx);
+}
+
+
+var AM = new AssetManager();
+AM.queueDownload("./img/ezPepe.jpg");
+
+AM.downloadAll(function() {
+	var canvas = document.getElementById('gameWorld');
+	var startButton = document.getElementById("startButton");
+	var clearButton = document.getElementById("clearButton");
+	var gosperButton = document.getElementById("gosperButton");
+	var randomButton = document.getElementById("randomButton");
+	var spaceshipButton = document.getElementById("spaceshipButton");
+
+	var sps = makeSpaceShip();
+
+    var ctx = canvas.getContext('2d');
+
+    var gameEngine = new GameEngine();
+    var grid = new Grid(gameEngine, null);
+    startButton.addEventListener("click", function () {
+    	grid.loaded = true;
+	}, false);
+	clearButton.addEventListener("click", function() {
+		grid.loaded = false;
+		grid.clear();
+	}, false);
+	gosperButton.addEventListener("click", function() {
+		grid.loaded = false;
+		grid.makeGosper();
+	}, false);
+	randomButton.addEventListener("click", function() {
+		grid.loaded = false;
+		grid.randomize();
+	}, false);
+	spaceshipButton.addEventListener("click", function() {
+		grid.loaded = false;
+		grid.clear();
+		grid.placeSetup(sps, 80, 20);
+		grid.placeSetup(sps, 75, 35);
+		grid.placeSetup(sps, 70, 50);
+
+	}, false);
+
+
+
+    gameEngine.addEntity(grid);
+
+  	console.log("Time to populate");
+    gameEngine.init(ctx);
+    gameEngine.start();
+});
